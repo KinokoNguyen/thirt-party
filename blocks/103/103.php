03aed0aa2c3846484343a5cc7ecff2f11666dbff
Minh Châu <pemidolly@gmail.com>
2017-03-01 16:15:30 +0700

    Template 103

--- blocks/103/103.php
+++ blocks/103/103.php
@@ -20,19 +20,38 @@
         <!--FOOTER-->
         <div class="copyright">
             <div class="container">
-                <div class="col-md-4">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <p><b>© Copyright 2017 L'amant Café. All rights reseved.</b></p>
                 </div>
-                <div class="col-md-8">
-                    <div class="logo-cert">
-                        <a href="#"><img src="images/android-app-store.png" alt=""/></a>
-                        <a href="#"><img src="images/app-store.png" alt=""/></a>
-                        <a href="#"><img src="images/haccp-certification-in-dubai.png" alt=""/></a>                     
-                        <a href="#"><img src="images/probat-black.jpg" alt=""/></a>                        
-                        <a href="#"><img src="images/brand.gif" alt=""/></a>                        
-                        <a href="#"><img src="images/rainforest-alliance-certified-seal-lg.png" alt=""/></a>                        
-                        <a href="#"><img src="images/vinh-hiep.png" alt=""/></a>
-                    </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                   <div class="dropdown">
+                    <div class="dropbtn">Show logo</div>
+                        <div class="logo-cert">
+                        <a href="#">
+                            <img src="images/android-app-store.png" alt=""/>
+                        </a>
+                        <a href="#">
+                            <img src="images/app-store.png" alt=""/>
+                        </a>
+                        <a href="#">
+                            <img src="images/haccp-certification-in-dubai.png" alt=""/>
+                        </a>                     
+                        <a href="#">
+                            <img src="images/probat-black.jpg" alt=""/>
+                        </a>                        
+                        <a href="#">
+                            <img src="images/brand.gif" alt=""/>
+                        </a>                        
+                        <a href="#">
+                            <img src="images/rainforest-alliance-certified-seal-lg.png" alt=""/>
+                        </a>                        
+                        <a href="#">
+                            <img src="images/vinh-hiep.png" alt=""/>
+                        </a>
+                        </div>
+                  </div> 
+                
+                    
                 </div>
             </div>
             <!--/END FOOTER-->
