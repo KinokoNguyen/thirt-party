de6e8047c868f051f76fba402184a7112d6f60c8
KinokoNguyen <maily27196@gmail.com>
2017-03-01 15:56:50 +0700

    Template module 100

--- blocks/100/100.php
+++ blocks/100/100.php
@@ -14,6 +14,7 @@
     ?>
     <link href="css/100.css" rel="stylesheet" type="text/css"/>
     <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script> 
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
 
 </head>
 
@@ -23,22 +24,27 @@
             <!--HEADER TOP-->
             <div class="header-top">
                 <div class="container">
-                    <div class="top-icons">
-                        <ul>
-                            <li><a href="#"><b>19002077 </b><span class="fa fa-phone" aria-hidden="true"></span></a></li>
-                            <li><a href="#"><b>Câu chuyện L'amant </b><span class="fa fa-comment" aria-hidden="true"></span></a> </li>
-                            <li><a href="#"><b>Giao hàng miễn phí </b><span class="fa fa-truck" aria-hidden="true"></span></a> </li>
-                            <li><a href="#"><b>Liên hệ </b><span class="fa fa-envelope" aria-hidden="true"></span></a> </li>
-                            <li><a href="#"><b>Trợ giúp </b><span class="fa fa-heart" aria-hidden="true"></span></a> </li>
-                            <li><a href="#"><b>Đăng ký hoặc đăng nhập </b><span class="fa fa-lock" aria-hidden="true"></span></a> </li>
-                            <li><a href="#"><b>Tìm kiếm </b><span class="fa fa-search"></span></a> </li>
-                            <li><a href="#"><b>En </b><span class="fa fa-chevron-circle-down" aria-hidden="true"></span></a> </li>
-                        </ul>
+                    <div class="dropdown">
+                        <div class="dropbtn"><a href="#"><b>Câu chuyện L'amant</b> <i class="fa fa-caret-square-o-down" aria-hidden="true"></i></a></div>
+                        <div class="dropdown-content">
+                            <div class="top-icons">
+                                <ul>
+                                    <li> <p class="fa fa-phone" aria-hidden="true">19002077</p></li>    
+                                    <li class="right"><a href="#"><b>Câu chuyện L'amant </b><span class="fa fa-comment" aria-hidden="true"></span></a> </li>
+                                    <li class="right"><a href="#"><b>Giao hàng miễn phí </b><span class="fa fa-truck" aria-hidden="true"></span></a> </li>
+                                    <li class="right"><a href="#"><b>Liên hệ </b><span class="fa fa-envelope" aria-hidden="true"></span></a> </li>
+                                    <li class="right"><a href="#"><b>Trợ giúp </b><span class="fa fa-heart" aria-hidden="true"></span></a> </li>
+                                    <li class="right"><a href="#"><b>Đăng ký / đăng nhập </b><span class="fa fa-lock" aria-hidden="true"></span></a> </li>
+                                    <li class="right"><a href="#"><b>Tìm kiếm </b><span class="fa fa-search"></span></a> </li>
+                                    <li class="right"><a href="#"><b>En </b><span class="fa fa-chevron-circle-down" aria-hidden="true"></span></a> </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <!--/END HEADER TOP-->
-            
+
         </div>
     </div>
 </body>
