324b705f568a408cda511977057a2a4758ef2729
KinokoNguyen <maily27196@gmail.com>
2017-03-23 08:34:10 +0700

    update

--- blocks/103/103_content.php
+++ blocks/103/103_content.php
@@ -1,7 +1,6 @@
 <?php
 $url_host = $_SERVER['HTTP_HOST'];
 
-
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
@@ -14,9 +13,13 @@ $url_path = $url_host . $matches[1][0];
     <!--FOOTER-->
     <div class="copyright">
         <div class="container">
+            <!--FOOTER LEFT-->
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <p><b>© Copyright 2017 L'amant Café. All rights reseved.</b></p>
             </div>
+            <!--/END FOOTER LEFT-->
+            
+            <!--FOOTER RIGHT-->
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 logo" style="text-align: center;">               
                 <div id="owl-demo" class="owl-carousel">
                     <div class="item"><img src="http://<?php echo $url_path ?>/images/google-play-badge.png" alt="Owl Image"></div>
@@ -30,11 +33,11 @@ $url_path = $url_host . $matches[1][0];
                     <div class="item"><img src="http://<?php echo $url_path ?>/images/google-play-badge.png" alt="Owl Image"></div>
                     <div class="item"><img src="http://<?php echo $url_path ?>/images/haccp.png" alt="Owl Image"></div>
                 </div>
-
             </div>
+            <!--/END FOOTER RIGHT-->
         </div>
-        <!--/END FOOTER-->
     </div>
+    <!--/END FOOTER-->
 </div>
 
 <script>
